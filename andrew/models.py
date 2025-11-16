d9cc64de4ce033004224ab713b772ec189165876
Andrew <alz9@njit.edu>
2025-11-16 04:48:37 -0500

    Hi

--- andrew/models.py
+++ andrew/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class GetMoney(models.Model):
+    userID = models.CharField(max_length=30)
+    budgetTot = models.CharField(max_length=30)
+    transTot = models.CharField(max_length=30)
 # Create your models here.
